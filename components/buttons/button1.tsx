47a57e3fcc79cf991deaaa4a5016241e899bd76f
twani <jbestius@gmail.com>
2025-03-12 13:27:36 +0300

    add the button

--- components/buttons/button1.tsx
+++ components/buttons/button1.tsx
@@ -2,10 +2,18 @@
 import { Icon } from "@iconify/react";
 import React from 'react'
 
-const ButtonMain = ({title, link}:any) => {
-  return (
-   link ? <a href={link} className='flex btn'>{title} <Icon className="-rotate-100 " icon="solar:arrow-down-broken" width="34" height="24" /></a> : <button className='flex btn'>{title} <Icon className="-rotate-100 " icon="solar:arrow-down-broken" width="34" height="24" /></button>
-  )
-}
+const ButtonMain = ({ link, title }:any) => {
+    return link ? (
+      <a href={link} className="flex btn group relative pr-[50px]">
+        {title}
+        <Icon className="absolute right-[17px] -rotate-100 group-hover:right-[10px] transition-all" icon="solar:arrow-down-broken" width="34" height="24" />
+      </a>
+    ) : (
+      <button className="flex btn">
+        {title}
+        <Icon className="absolute  right-[17px] -rotate-100 group-hover:right-[10px] transition-all" icon="solar:arrow-down-broken" width="34" height="24" />
+      </button>
+    );
+  };
 
-export default ButtonMain
+  export default ButtonMain
