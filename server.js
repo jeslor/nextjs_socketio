dbe32f0771ccbc52ad47d047501379e6238e99fe
twani <jbestius@gmail.com>
2025-03-19 13:05:30 +0300

    adjust the host and port

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 import { createServer } from "node:http";
 import next from "next";
 import { Server } from "socket.io";
+import dotenv from "dotenv";
 
+dotenv.config();
 console.log("process.env.PORT", process.env.PORT);
 
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
-const port = process.env.PORT;
+const hostname = "https://next-chat-app-5npg.onrender.com";
+const port = process.env.PORT || 3000;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
 
 
 app.prepare().then(() => {
+
   const httpServer = createServer(handler);
 
    const io = new Server(httpServer,{
