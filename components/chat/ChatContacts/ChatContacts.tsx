a6503a4598eb7e1cd63354fa2255e464b9c42b38
Jeslor Ssozi <56073031+jeslor@users.noreply.github.com>
2025-04-02 19:31:13 +0200

    add the logic for filtering users and other users

--- components/chat/ChatContacts/ChatContacts.tsx
+++ components/chat/ChatContacts/ChatContacts.tsx
@@ -7,21 +7,27 @@ import ContactSkeleton from '../ContactSkeleton/ContactSkeleton';
 const ChatContacts = memo(({expand, searchQuery}:{expand:boolean, searchQuery:string}) => {
     const {contacts, isLoadingContacts, currentUser} = useCurrentUserStore();
     const [currContacts, setCurrContacts] = React.useState<any[]>([]);
+    const [yourContacts, setYourContacts] = React.useState<any[]>([]);
+    const [otherContacts, setOtherContacts] = React.useState<any[]>([]);
     
 
     useEffect(() => {
+      let currentUserContactIds = currentUser?.contacts.map((contactId: any) => contactId._id);
         if(contacts.length>0){
-          if(searchQuery.length>0){
-            searchQuery = searchQuery.toLowerCase();
-            const filteredContacts = contacts.filter((contact:any) => {
-              const contactName = contact.username.toLowerCase();
-              const contactEmail = contact.email.toLowerCase();
-              return contactName.includes(searchQuery) || contactEmail.includes(searchQuery);
-            });
-            setCurrContacts(filteredContacts);
-          }else{
-          setCurrContacts(contacts);
-          }
+         if(searchQuery.length>0){
+          let LowerCaseSearchQuery = searchQuery.toLowerCase();
+          setYourContacts(contacts.filter((contact: any) => {
+            return (contact.username.toLowerCase().includes(LowerCaseSearchQuery)|| contact.email.toLowerCase().includes(LowerCaseSearchQuery)) && currentUserContactIds.includes(contact._id)
+           }))
+          setOtherContacts(contacts.filter((contact: any) => {
+            return (contact.username.toLowerCase().includes(LowerCaseSearchQuery)|| contact.email.toLowerCase().includes(LowerCaseSearchQuery)) && !currentUserContactIds.includes(contact._id)
+           }))
+         
+         
+         }else{
+          setYourContacts(contacts.filter((contact: any) => currentUserContactIds.includes(contact._id)))
+          setOtherContacts(contacts.filter((contact: any) => !currentUserContactIds.includes(contact._id)))
+         }
         }
     }
     ,[contacts, searchQuery])
@@ -31,9 +37,17 @@ const ChatContacts = memo(({expand, searchQuery}:{expand:boolean, searchQuery:st
     <div className="px-4 flex flex-col gap-y-2 py-4 h-[calc(100vh-70px)] overflow-y-scroll pb-7">
     {isLoadingContacts
     ? <ContactSkeleton /> 
-    :currContacts.map((contact: any) => <ContactCard key={contact._id} contact={contact} expand={expand} />)}
+    :<>
+    <h3 className='font-bold'>YourContacts</h3>
+    {yourContacts.length > 0 ? yourContacts.map((contact: any) => <ContactCard key={contact._id} contact={contact} expand={expand} />) : <p className='text-gray-500'>No contacts found</p>}
+    <h3 className='font-bold'>Other users</h3>
+    {otherContacts.length > 0 ? otherContacts.map((contact: any) => <ContactCard key={contact._id} contact={contact} expand={expand} />) : <p className='text-gray-500'>No contacts found</p>}
+    </>}
     </div>
   )
 })
 
 export default ChatContacts
+
+
+// currContacts.map((contact: any) => <ContactCard key={contact._id} contact={contact} expand={expand} />
