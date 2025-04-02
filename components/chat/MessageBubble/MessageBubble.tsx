6bba4e8c3ecde69ef8f280dea4bcd6eec1b126cf
Jeslor Ssozi <56073031+jeslor@users.noreply.github.com>
2025-04-02 16:34:08 +0200

    adjust the background color of the chat bubble image

--- components/chat/MessageBubble/MessageBubble.tsx
+++ components/chat/MessageBubble/MessageBubble.tsx
@@ -45,7 +45,7 @@ const MessageBubble = ({ message }: any) => {
       </div>
       <div className={`chat-bubble ${message.sender._id === currentUser._id ?'bg-base-200 text-primary/90':'bg-primary/60 text-base-200'}`}>
         {message.file && (
-          <div className="flex flex-col items-center gap-2 h-[150px]">
+          <div className="flex flex-col items-center gap-2 h-[150px] bg-white rounded-[10px] mb-2">
             <img
               src={message.file}
               alt="file"
