9687cc5ede2d54e25b98421b44a750ea7b8afbf4
Jeslor Ssozi <56073031+jeslor@users.noreply.github.com>
2025-04-04 14:04:53 +0200

    add the error message accrording to where they happened'

--- lib/actions/message.actions.ts
+++ lib/actions/message.actions.ts
@@ -1,137 +1,203 @@
 "use server";
 import { ConnectToDB } from "@/lib/mongoose";
-import Message  from "@/lib/models/message.model";
+import Message from "@/lib/models/message.model";
 import UserModel from "@/lib/models/user.model";
 import { uploadCloudinary } from "@/lib/helper/cloudinary";
 
-
-export const newMessage = async ({senderId, receiverId, text, file}:{
-    senderId:string,
-    receiverId:string,
-    text:string,
-    file?:any
+export const newMessage = async ({
+  senderId,
+  receiverId,
+  text,
+  file,
+}: {
+  senderId: string;
+  receiverId: string;
+  text: string;
+  file?: any;
 }) => {
-    try {
-        await ConnectToDB();
-        let  image = {secure_url:""};
-       if (file) {
-         await uploadCloudinary(file, "message").then((result) => {
-            if (result && 'secure_url' in result) {
-                image = { secure_url: result.secure_url };
-            } else {
-                throw new Error("Invalid response from uploadCloudinary");
-            }
-        }).catch((error) => {         
-            console.log(error);
-            throw new Error("Error uploading image");
+  try {
+    await ConnectToDB();
+    let image = { secure_url: "" };
+    if (file) {
+      await uploadCloudinary(file, "message")
+        .then((result) => {
+          if (result && "secure_url" in result) {
+            image = { secure_url: result.secure_url };
+          } else {
+            throw new Error("Invalid response from uploadCloudinary");
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          throw new Error("Error uploading image");
         });
-       }
-        const message = new Message({
-            sender:senderId,
-            receiver:receiverId,
-            text,
-            file:image.secure_url,
-        });
-
-        await message.save();
-        const updatedMessage = await Message.findOne({_id:message._id}).populate("sender receiver");
-        
-        
-       
-        return JSON.parse(JSON.stringify({status:200, message: "Message Sent", data:updatedMessage}));
-    } catch (error) {
-        console.log(error);
-        return JSON.parse(JSON.stringify({status:500, message: "Internal Server Error", data:error}));
     }
-}
+    const message = new Message({
+      sender: senderId,
+      receiver: receiverId,
+      text,
+      file: image.secure_url,
+    });
 
-export const getMessages = async (loggedInUserId:string, contactId:string) => {
-    try {
-        await ConnectToDB();
-        const messages = await Message.find({
-            $and: [
-                { 
-                  $or: [
-                    { sender: loggedInUserId, receiver: contactId },
-                    { sender: contactId, receiver: loggedInUserId }
-                  ]
-                },
-                { deleted: false },
-                {$or:[
-                    { sender: loggedInUserId },
-                    { seen: true }
-                ]}
-              ]
-        }).populate("sender receiver");
-        return JSON.parse(JSON.stringify({status:200, message: "Messages Found", data:messages}));
-    } catch (error) {
-        console.log(error);
-        return JSON.parse(JSON.stringify({status:500, message: "Internal Server Error", data:error}));
-        
+    await message.save();
+    const updatedMessage = await Message.findOne({ _id: message._id }).populate(
+      "sender receiver"
+    );
+
+    return JSON.parse(
+      JSON.stringify({
+        status: 200,
+        message: "Message Sent",
+        data: updatedMessage,
+      })
+    );
+  } catch (error) {
+    console.log(error);
+    return JSON.parse(
+      JSON.stringify({
+        status: 500,
+        message: "Internal Server Error new message",
+        data: error,
+      })
+    );
+  }
+};
+
+export const getMessages = async (
+  loggedInUserId: string,
+  contactId: string
+) => {
+  try {
+    await ConnectToDB();
+    const messages = await Message.find({
+      $and: [
+        {
+          $or: [
+            { sender: loggedInUserId, receiver: contactId },
+            { sender: contactId, receiver: loggedInUserId },
+          ],
+        },
+        { deleted: false },
+        { $or: [{ sender: loggedInUserId }, { seen: true }] },
+      ],
+    }).populate("sender receiver");
+    return JSON.parse(
+      JSON.stringify({ status: 200, message: "Messages Found", data: messages })
+    );
+  } catch (error) {
+    console.log(error);
+    return JSON.parse(
+      JSON.stringify({
+        status: 500,
+        message: "Internal Server Error get messages",
+        data: error,
+      })
+    );
+  }
+};
+
+export const unReadMessages = async (loggedInUserId: string, message: any) => {
+  try {
+    await ConnectToDB();
+    const updatedLoggedInUser = await UserModel.findOneAndUpdate(
+      { _id: loggedInUserId },
+      {
+        $push: {
+          unreadMessages: message,
+        },
+      },
+      { new: true }
+    ).populate("unreadMessages");
+    return JSON.parse(
+      JSON.stringify({
+        status: 200,
+        message: "Message added to unread",
+        data: updatedLoggedInUser,
+      })
+    );
+  } catch (error) {
+    console.log(error);
+    return JSON.parse(
+      JSON.stringify({
+        status: 500,
+        message: "error in adding message to unread",
+        data: error,
+      })
+    );
+  }
+};
+
+export const updateReadMessages = async (
+  loggedInUserId: string,
+  messageIds: string[]
+) => {
+  try {
+    await ConnectToDB();
+    const updateReadMessages = await Message.updateMany(
+      { _id: { $in: messageIds } },
+      { $set: { seen: true } },
+      { new: true }
+    );
+
+    const updatedLoggedInUser = await UserModel.findOneAndUpdate(
+      { _id: loggedInUserId },
+      {
+        $pull: {
+          unreadMessages: { $in: messageIds },
+        },
+      },
+      { new: true }
+    ).populate("unreadMessages");
+
+    if (!updatedLoggedInUser) {
+      return JSON.parse(
+        JSON.stringify({ status: 404, message: "User not found", data: null })
+      );
     }
-}
-
-
-export const unReadMessages = async (loggedInUserId: string, message: any)  => {
-
-    try {
-        await ConnectToDB();
-        const updatedLoggedInUser = await UserModel.findOneAndUpdate(
-            { _id: loggedInUserId },
-            {
-                $push: {
-                    unreadMessages: message
-                }
-            },
-            { new: true }
-        ).populate("unreadMessages");
-        return JSON.parse(JSON.stringify({ status: 200, message: "Message added to unread", data: updatedLoggedInUser }));
-    } catch (error) {
-        console.log(error);
-        return JSON.parse(JSON.stringify({ status: 500, message: "error in adding message to unread", data: error }));
+    if (updateReadMessages.modifiedCount === 0) {
+      return JSON.parse(
+        JSON.stringify({
+          status: 404,
+          message: "No messages found",
+          data: null,
+        })
+      );
     }
-}
-
-export const updateReadMessages = async (loggedInUserId: string, messageIds: string[]) => {
-    try {
-        await ConnectToDB();
-        const updateReadMessages = await Message.updateMany(
-            { _id: { $in: messageIds } },
-            { $set: { seen: true } },
-            { new: true }
-        );
-
-        
-
-        const updatedLoggedInUser = await UserModel.findOneAndUpdate(
-            { _id: loggedInUserId },
-            {
-                $pull: {
-                    unreadMessages: { $in: messageIds }
-                }
-            },
-            { new: true }
-        ).populate("unreadMessages");
-        
-
-        if (!updatedLoggedInUser) {
-            return JSON.parse(JSON.stringify({ status: 404, message: "User not found", data: null }));
-        }
-        if (updateReadMessages.modifiedCount === 0) {
-            return JSON.parse(JSON.stringify({ status: 404, message: "No messages found", data: null }));
-        }
-        if (updateReadMessages.modifiedCount !== messageIds.length) {
-            return JSON.parse(JSON.stringify({ status: 404, message: "Some messages not found", data: null }));
-        }
-        if (updateReadMessages.modifiedCount === messageIds.length) {
-            return JSON.parse(JSON.stringify({ status: 200, message: "All messages updated to seen", data: updatedLoggedInUser }));
-        }
-        if (updateReadMessages.modifiedCount > 0) {
-            return JSON.parse(JSON.stringify({ status: 200, message: "Some messages updated to seen", data: updatedLoggedInUser }));
-        }
-    } catch (error) {
-        console.log(error);
-        return JSON.parse(JSON.stringify({ status: 500, message: "Internal Server Error", data: error }));
+    if (updateReadMessages.modifiedCount !== messageIds.length) {
+      return JSON.parse(
+        JSON.stringify({
+          status: 404,
+          message: "Some messages not found",
+          data: null,
+        })
+      );
     }
-}
-
+    if (updateReadMessages.modifiedCount === messageIds.length) {
+      return JSON.parse(
+        JSON.stringify({
+          status: 200,
+          message: "All messages updated to seen",
+          data: updatedLoggedInUser,
+        })
+      );
+    }
+    if (updateReadMessages.modifiedCount > 0) {
+      return JSON.parse(
+        JSON.stringify({
+          status: 200,
+          message: "Some messages updated to seen",
+          data: updatedLoggedInUser,
+        })
+      );
+    }
+  } catch (error) {
+    console.log(error);
+    return JSON.parse(
+      JSON.stringify({
+        status: 500,
+        message: "Internal Server Error update read messages",
+        data: error,
+      })
+    );
+  }
+};
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,78 +1,119 @@
-"use server"
-import { ConnectToDB } from "../mongoose"
-import UserModel from "@/lib/models/user.model"
-import MessageModel from "@/lib/models/message.model"
-
-
+"use server";
+import { ConnectToDB } from "../mongoose";
+import UserModel from "@/lib/models/user.model";
+import MessageModel from "@/lib/models/message.model";
 
 export const getCurrentUser = async (email: string) => {
-    
-    try {
-        await ConnectToDB()
-        const user = await UserModel.findOne({email})
-        .select("-password")
-        .populate("contacts", "-password")
-        .populate({
-            path:"unreadMessages",
-            model:MessageModel,
-            populate:{
-                path:"sender receiver",
-                model:UserModel,
-                select:"-password"
-            }
+  try {
+    await ConnectToDB();
+    const user = await UserModel.findOne({ email })
+      .select("-password")
+      .populate("contacts", "-password")
+      .populate({
+        path: "unreadMessages",
+        model: MessageModel,
+        populate: {
+          path: "sender receiver",
+          model: UserModel,
+          select: "-password",
+        },
+      });
+
+    if (!user) {
+      return JSON.parse(
+        JSON.stringify({ status: 404, message: "User Not Found", data: {} })
+      );
+    }
+    return JSON.parse(
+      JSON.stringify({ status: 200, message: "User Found", data: user })
+    );
+  } catch (error) {
+    console.log(error);
+
+    return JSON.parse(
+      JSON.stringify({
+        status: 500,
+        message: "Internal Server Error get current user",
+        data: error,
+      })
+    );
+  }
+};
+
+export const getOtherUsers = async (userId: string) => {
+  try {
+    await ConnectToDB();
+    const users = await UserModel.find({
+      _id: { $ne: userId },
+      $or: [
+        { "privacySettings.noFindingMe": false }, // Users who have noFindingMe set to false
+        { "privacySettings.noFindingMe": { $exists: false } }, // Users where noFindingMe is undefined
+      ],
+    }).select("-password");
+
+    return JSON.parse(
+      JSON.stringify({ status: 200, message: "Users Found", data: users })
+    );
+  } catch (error) {
+    console.log(error);
+    return JSON.parse(
+      JSON.stringify({
+        status: 500,
+        message: "Internal Server Error get other users",
+        data: error,
+      })
+    );
+  }
+};
+
+export const updateUser = async (userId: string, data: any) => {
+  try {
+    console.log("Updating user", userId, data);
+
+    await ConnectToDB();
+    const user = await UserModel.findByIdAndUpdate(userId, data, {
+      new: true,
+    }).select("-password");
+    return JSON.parse(
+      JSON.stringify({ status: 200, message: "User Updated", data: user })
+    );
+  } catch (error) {
+    console.log(error);
+    return JSON.parse(
+      JSON.stringify({
+        status: 500,
+        message: "Internal Server Error update user",
+        data: error,
+      })
+    );
+  }
+};
+
+export const addToContacts = async (userId: string, contactId: string) => {
+  try {
+    const addedToContacts = await UserModel.findByIdAndUpdate(
+      userId,
+      { $addToSet: { contacts: contactId } },
+      { new: true }
+    ).select("-password");
+    if (addedToContacts) {
+      return JSON.parse(
+        JSON.stringify({
+          status: 200,
+          message: "Contact Added",
+          data: addedToContacts,
         })
-        
-        if(!user){
-            return JSON.parse(JSON.stringify({status:404, message: "User Not Found", data:{}}))
-        }
-        return JSON.parse(JSON.stringify({status:200, message: "User Found", data:user}))
-    } catch (error) {
-        console.log(error);
-        
-        return JSON.parse(JSON.stringify({status:500, message: "Internal Server Error", data:error}))
+      );
+    } else {
+      throw new Error("Error adding contact");
     }
-}
-
-export const getOtherUsers = async (userId:string) => {
-    try {
-        await ConnectToDB()
-        const users = await UserModel.find({ _id: { $ne: userId }, 
-            $or: [
-                { "privacySettings.noFindingMe": false }, // Users who have noFindingMe set to false
-                { "privacySettings.noFindingMe": { $exists: false } } // Users where noFindingMe is undefined
-            ]
-    }).select("-password")
-        
-        return JSON.parse(JSON.stringify({status:200, message: "Users Found", data:users}))
-    } catch (error) {
-        console.log(error);
-        return JSON.parse(JSON.stringify({status:500, message: "Internal Server Error", data:error}))
-    }
-}
-
-export const updateUser = async (userId:string, data:any) => {
-    try {
-        console.log("Updating user", userId, data);
-        
-        
-        await ConnectToDB()
-        const user = await UserModel.findByIdAndUpdate(userId, data, {new:true}).select("-password")
-        return JSON.parse(JSON.stringify({status:200, message: "User Updated", data:user}))
-    } catch (error) {
-        console.log(error);
-        return JSON.parse(JSON.stringify({status:500, message: "Internal Server Error", data:error}))
-    }
-}
-
-export const addToContacts = async (userId:string, contactId:string) => {
-    try {
-        const addedToContacts = await UserModel.findByIdAndUpdate(userId, {$addToSet:{contacts:contactId}}, {new:true}).select("-password")
-        if(addedToContacts){
-            return JSON.parse(JSON.stringify({status:200, message: "Contact Added", data:addedToContacts}))
-        }else{
-            throw new Error("Error adding contact")
-        }
-    } catch (error) {
-        return JSON.parse(JSON.stringify({status:500, message: "Internal Server Error", data:error}))
-    }
-}
+  } catch (error) {
+    return JSON.parse(
+      JSON.stringify({
+        status: 500,
+        message: "Internal Server Error add to contacts",
+        data: error,
+      })
+    );
+  }
+};
