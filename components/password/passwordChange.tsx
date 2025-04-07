c48607a9907a73132a956bd42288d667546dc2d7
Jeslor Ssozi <56073031+jeslor@users.noreply.github.com>
2025-04-07 11:09:24 +0200

    add logic to change current password frontEnd

--- components/password/passwordChange.tsx
+++ components/password/passwordChange.tsx
@@ -11,12 +11,33 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 interface Props {
   handleCloseChangePassword: () => void;
 }
 
 const PasswordChange = ({ handleCloseChangePassword }: Props) => {
+  const [isCurrentPassword, setIsCurrentPassword] = useState(false);
+  const [currPassword, setCurrPassword] = useState("");
+
+  const checkCurrentPassword = async () => {
+    const isPasswordValid: any = await fetch("/api/check_password", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ currPassword }),
+    });
+    if (isPasswordValid.status === 200) {
+      if (isPasswordValid.confirmedPassword) {
+        setIsCurrentPassword(true);
+      }
+      if (!isPasswordValid.confirmedPassword) {
+        setIsCurrentPassword(false);
+      }
+    }
+  };
   return (
     <div className=" h-screen w-screen flex justify-center items-center bg-slate-100 fixed left-0 top-0 z-[100]">
       <div className="w-full max-w-[500px] bg-white rounded-lg shadow-lg p-8 relative">
@@ -28,14 +49,22 @@ const PasswordChange = ({ handleCloseChangePassword }: Props) => {
           />
         </button>
         <h3 className="font-semibold text-[1.3rem]">Change Password</h3>
-        <div>
+        <div className="flex flex-col items-start gap-y-2 justify-center mt-5 ">
           <Input
             placeholder="Current Password"
             type="password"
             className="w-full mt-4"
+            onChange={(e) => setCurrPassword(e.target.value)}
+            value={currPassword}
           />
-
-          <button>submit</button>
+          {!isCurrentPassword && (
+            <button
+              onClick={() => checkCurrentPassword}
+              className="bg-primary7 text-white font-bold py-2 px-4  rounded-full hover:bg-primary8 transition duration-200 cursor-pointer"
+            >
+              submit
+            </button>
+          )}
         </div>
       </div>
     </div>
