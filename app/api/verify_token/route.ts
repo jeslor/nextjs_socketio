d2c202bdae3d8773dda812a53e88ec31e7a8f039
Jeslor Ssozi <56073031+jeslor@users.noreply.github.com>
2025-04-07 16:35:56 +0200

    add route to verify token at start

---
+++ app/api/verify_token/route.ts
@@ -0,0 +1,41 @@
+import UserModel from "@/lib/models/user.model";
+import { ConnectToDB } from "@/lib/mongoose";
+import { NextResponse } from "next/server";
+import crypto from "crypto";
+
+export async function POST(request: Request) {
+  const { token } = await request.json();
+
+  if (!token) {
+    return NextResponse.json({ message: "Token is required" }, { status: 400 });
+  }
+
+  try {
+    await ConnectToDB();
+    const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
+    const user = await UserModel.findOne({
+      passwordToken: hashedToken,
+      $gt: {
+        passwordTokenExpiry: Date.now(),
+      },
+    });
+    if (!user) {
+      return NextResponse.json({
+        message: "Invalid or expired token",
+        status: 400,
+      });
+    }
+
+    return NextResponse.json({
+      message: "Token is valid",
+      status: 200,
+      userEmail: user.email,
+    });
+  } catch (error) {
+    console.error("Error verifying token:", error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
