d3d0eebcc11c53dadd97983ac09114f8ab748586
Jeslor Ssozi <56073031+jeslor@users.noreply.github.com>
2025-04-07 11:16:13 +0200

    add route to handle check password

---
+++ app/api/check_password/route.ts
@@ -0,0 +1,46 @@
+import UserModel from "@/lib/models/user.model";
+import { ConnectToDB } from "@/lib/mongoose";
+import { NextResponse } from "next/server";
+import { compare } from "bcryptjs";
+
+export async function POST(request: Request) {
+  const { email, password } = await request.json();
+  if (!email || !password) {
+    return NextResponse.json(
+      { message: "Email and password are required" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    await ConnectToDB();
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        { message: "Invalid email or password" },
+        { status: 401 }
+      );
+    }
+
+    const isPasswordValid = await compare(password, user.password);
+    if (!isPasswordValid) {
+      return NextResponse.json(
+        { message: "Password incorrect", confirmedPassword: false },
+        { status: 200 }
+      );
+    }
+
+    if (isPasswordValid) {
+      return NextResponse.json(
+        { message: "Password is correct", confirmedPassword: true },
+        { status: 200 }
+      );
+    }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
